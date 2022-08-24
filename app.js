e7b3785530ec39df36a3fefd572cf080f3f8c592
Luam Ahrion <luam_azf@hotmail.com>
2022-08-24 18:54:45 +0000

    cria transacoes-repositorio

--- app.js
+++ app.js
@@ -1,13 +1,31 @@
 const express = require('express')
+
+const TransacoesRepositorio = require("./transacoes-repositorio")
+
 const app = express()
 const port = 3000;
 
 app.use(express.static(`${__dirname}/public`))
 
-app.get('/ola',(req,res) => {
-    res.send("ola mundo!")
+app.get('/transacoes', (req, res) => {
+  const repositorio = new TransacoesRepositorio()
+  const transacoes = repositorio.listarTransacoes()
+  res.send(transacoes)
 })
 
-server.listen(port, () => {
+app.get('/criartransacao', (req, res) => {
+
+  const repositorio = new TransacoesRepositorio()
+
+  const transacao = {
+    valor: 10,
+    descricao: "bolo"
+  }
+
+  const transacoes = repositorio.criarTransacao(transacao)
+  res.send(transacoes)
+})
+
+app.listen(port, () => {
   console.log(`Servidor ouvindo na porta ${port}`);
 });
---
+++ transacoes-repositorio.js
@@ -0,0 +1,29 @@
+const transacoes = {
+
+    transacoes: [
+        {
+            valor: 10,
+            descricao: "Pastel"
+        },
+        {
+            valor: 3,
+            descricao: "Doce"
+        }
+    ]
+}
+
+class TransacoesRepositorio {
+
+    listarTransacoes() {
+        return transacoes
+    }
+
+    criarTransacao(transacao) {
+        const lista = transacoes.transacoes
+        lista.push(transacao)
+        return lista
+    }
+
+}
+
+module.exports = TransacoesRepositorio

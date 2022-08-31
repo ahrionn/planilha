d87aabf91c1dcb256498e056c0cfd693198668ae
Luam Ahrion <luam_azf@hotmail.com>
2022-08-31 19:01:37 +0000

    add outra porta p/ servidor ouvir

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ require('dotenv/config')
 const express = require('express')
 const TransacoesRepositorio = require("./infra/sql-transacoes-repositorio")
 const app = express()
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.static(`${__dirname}/public`))
 
